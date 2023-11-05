3e23d71b003aa9d2534d37419d14a7f170eb4e48
Joffrey <joffrey.mischlerps@gmail.com>
2023-11-05 13:30:46 +0100

    fix exports

--- index.ts
+++ index.ts
@@ -2,5 +2,4 @@ export {Game} from "./Game";
 export {ActionsEnum} from "./enums/actions.enum";
 export {ColorEnum} from "./enums/color.enum";
 
-export {PlayerStateDTO} from "./types/PlayerStateDTO";
-export {TetriminoPieceDTO} from "./types/PlayerStateDTO";
+export {PlayerStateDTO, TetriminoPieceDTO} from "./types/PlayerStateDTO";
