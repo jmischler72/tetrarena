c3ddcd0a5d4e24b794c71382289f57a5b7f12a5d
Joffrey <joffrey.mischlerps@gmail.com>
2023-11-06 07:49:41 +0100

    Revert "separate singleplayer game and multi"
    This reverts commit c644a1ce

--- Game.ts
+++ Game.ts
@@ -1,16 +1,40 @@
 import { PlayerGameState } from './PlayerGameState';
 import { ActionsEnum } from './enums/actions.enum';
-
+import { GAME_SPEED } from './constants/game';
+import {PlayerStateDTO} from "./types/PlayerStateDTO";
 
 export class Game {
-  protected readonly playerGameState: PlayerGameState = new PlayerGameState();
+  private gameTimer: null | ReturnType<typeof setTimeout> = null;
+  public readonly playerGameState: PlayerGameState = new PlayerGameState();
 
-  public handleAction(action: ActionsEnum){
-    this.playerGameState.handleAction(action);
+  constructor(private callback: (playerState: PlayerStateDTO)=> void) {
+    console.log('Game Started');
   }
 
-  public updateGame(){
-    this.playerGameState.handleAction(ActionsEnum.GO_DOWN);
+  private callbackOnPlayerStateUpdate() {
+    this.callback(this.playerGameState.getPlayerState());
+    this.playerGameState.deletedLines = [];
+    this.playerGameState.currentTetriminoFreezed = false;
+  }
+
+  handleAction(action: ActionsEnum) {
+    this.playerGameState.handleAction(action);
+    this.callbackOnPlayerStateUpdate();
+  }
+
+  startGame(){
+    this.updateGame();
+  }
+
+  private updateGame() {
+    this.gameTimer = setTimeout(this.updateGame.bind(this), GAME_SPEED); // https://stackoverflow.com/a/5911280
+
+    if (!this.playerGameState.isGameOver) {
+      this.playerGameState.handleAction(ActionsEnum.GO_DOWN);
+      this.callbackOnPlayerStateUpdate();
+    } else {
+      clearTimeout(this.gameTimer);
+    }
   }
 
   public getPlayerGameState(){
