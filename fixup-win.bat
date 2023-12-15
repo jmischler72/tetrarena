f02c3535fc1df45092709db9fc5feb11c37a88cd
Joffrey <joffrey.mischlerps@gmail.com>
2023-12-16 00:43:59 +0100

    remove scripts

--- fixup-win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo {> dist/cjs/package.json
-echo    "type": "commonjs" >> dist/cjs/package.json
-echo }>> dist/cjs/package.json
-
-echo {> dist/mjs/package.json
-echo    "type": "module" >> dist/mjs/package.json
-echo }>> dist/mjs/package.json
