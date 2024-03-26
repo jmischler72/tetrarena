d22821c45af6736cba84dc86daa2616855125045
Joffrey <joffrey.mischlerps@gmail.com>
2024-03-26 09:17:50 +0100

    refacto routes + clean code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,7 @@
     import Home from "./Home.svelte";
 </script>
 
-<Home></Home>
+<div class="flex flex-col gap-y-14 items-center">
+    <h1 class="text-gray-200 text-5xl border-solid border-2 border-white p-1">tetrarena</h1>
+    <Home></Home>
+</div>
