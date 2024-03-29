d03b8e38ddaa7eddc20c1f1d67bea5cd62e7ac5b
Joffrey <joffrey.mischlerps@gmail.com>
2024-03-29 13:33:24 +0100

    small adjustment menu

--- src/lib/components/menu/MenuWithNavbar.svelte
+++ src/lib/components/menu/MenuWithNavbar.svelte
@@ -4,7 +4,7 @@ import Navbar from "../../../routes/(menu)/Navbar.svelte";
 
 <div class="h-full w-full flex flex-col gap-6 p-8 ">
     <Navbar/>
-    <div class="h-[90%] w-full flex justify-center items-center">
+    <div class="flex flex-col bg-black/[.6] text-white rounded h-[90%] w-full z-10 overflow-hidden justify-between">
         <slot/>
     </div>
 </div>
