0840a0d05fdeca63e3892b31357edea3b9f3fe89
Joffrey <joffrey.mischlerps@gmail.com>
2024-03-30 12:34:56 +0100

    clean code prettier

--- src/TetrisPixi/scenes/MultiPlayerGameScene.ts
+++ src/TetrisPixi/scenes/MultiPlayerGameScene.ts
@@ -1,70 +1,75 @@
-import GameScene from './GameScene';
-import BoardContainer from '../components/BoardContainer/BoardContainer';
-import * as TWEEN from '@tweenjs/tween.js';
-import {get} from 'svelte/store';
+import GameScene from './GameScene'
+import BoardContainer from '../components/BoardContainer/BoardContainer'
+import * as TWEEN from '@tweenjs/tween.js'
+import {get} from 'svelte/store'
 
-import type {IScene} from "../Manager";
-import {gameStatesStore, roomStore} from "$lib/stores/multiplayerStore";
-import type {GameStateDTO} from "@jmischler72/core-tetris";
+import type {IScene} from '../Manager'
+import {gameStatesStore, roomStore} from '$lib/stores/multiplayerStore'
+import type {GameStateDTO} from '@jmischler72/core-tetris'
 
 export default class MultiPlayerGameScene extends GameScene implements IScene {
-    private readonly playerBoard: BoardContainer;
-    private readonly oppBoard: BoardContainer;
-    private isGameOver: boolean = false;
+	private readonly playerBoard: BoardContainer
+	private readonly oppBoard: BoardContainer
+	private isGameOver: boolean = false
 
-    constructor() {
-        super();
+	constructor() {
+		super()
 
-        this.playerBoard = new BoardContainer();
-        this.oppBoard = new BoardContainer();
-        this.oppBoard.position.set(
-            this.playerBoard.x + this.playerBoard.width,
-            this.playerBoard.y
-        );
-        this.addChild(this.playerBoard, this.oppBoard);
+		this.playerBoard = new BoardContainer()
+		this.oppBoard = new BoardContainer()
+		this.oppBoard.position.set(
+			this.playerBoard.x + this.playerBoard.width,
+			this.playerBoard.y
+		)
+		this.addChild(this.playerBoard, this.oppBoard)
 
-        this.playerBoard.renderPlayerBorder();
-    }
+		this.playerBoard.renderPlayerBorder()
+	}
 
-    update(): void {
-        this.stats.begin();
-        TWEEN.update();
+	update(): void {
+		this.stats.begin()
+		TWEEN.update()
 
-        const gameStates: Map<string, GameStateDTO> = get(gameStatesStore);
+		const gameStates: Map<string, GameStateDTO> = get(gameStatesStore)
 
-        if (gameStates && !this.isGameOver) {
-            gameStates.forEach((value: GameStateDTO, key: string) => {
-                if (key === get(roomStore)?.sessionId) {
-                    this.playerBoard.updatePlayerBoard(value);
-                } else {
-                    this.oppBoard.updatePlayerBoard(value);
-                }
-                // if (gameState.isGameOver) {
-                //     this.isGameOver = true;
-                //     if (key == socketId) {
-                //         this.playerBoard.gameOverAnimation();
-                //     } else {
-                //         this.oppBoard.gameOverAnimation();
-                //     }
-                // }
-            });
-        }
+		if (gameStates && !this.isGameOver) {
+			gameStates.forEach((value: GameStateDTO, key: string) => {
+				if (key === get(roomStore)?.sessionId) {
+					this.playerBoard.updatePlayerBoard(value)
+				} else {
+					this.oppBoard.updatePlayerBoard(value)
+				}
+				// if (gameState.isGameOver) {
+				//     this.isGameOver = true;
+				//     if (key == socketId) {
+				//         this.playerBoard.gameOverAnimation();
+				//     } else {
+				//         this.oppBoard.gameOverAnimation();
+				//     }
+				// }
+			})
+		}
 
-        this.stats.end();
-    }
+		this.stats.end()
+	}
 
-    resize(screenWidth: number, screenHeight: number): void {
+	resize(screenWidth: number, screenHeight: number): void {
+		const GAMES_CONTAINER_GAP: number = 60
 
-        const GAMES_CONTAINER_GAP: number = 60;
+		this.playerBoard.position.set(
+			screenWidth / 2 -
+				this.playerBoard.width / 2 -
+				this.oppBoard.width / 2 -
+				GAMES_CONTAINER_GAP,
+			screenHeight / 2 - this.playerBoard.height / 2
+		)
 
-        this.playerBoard.position.set(
-            screenWidth / 2 - this.playerBoard.width / 2 - this.oppBoard.width / 2 - GAMES_CONTAINER_GAP,
-            screenHeight / 2 - this.playerBoard.height / 2
-        );
-
-        this.oppBoard.position.set(
-            screenWidth / 2 - this.oppBoard.width / 2 + this.playerBoard.width / 2 + GAMES_CONTAINER_GAP,
-            screenHeight / 2 - this.oppBoard.height / 2
-        );
-    }
+		this.oppBoard.position.set(
+			screenWidth / 2 -
+				this.oppBoard.width / 2 +
+				this.playerBoard.width / 2 +
+				GAMES_CONTAINER_GAP,
+			screenHeight / 2 - this.oppBoard.height / 2
+		)
+	}
 }
