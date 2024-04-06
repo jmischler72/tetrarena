8f3a26624f974ecea7c9d96386a81397e1286381
Joffrey <joffrey.mischlerps@gmail.com>
2024-04-06 15:14:17 +0200

    feat(client): allow reconnection (refresh ...)

--- apps/client/src/lib/functions/services/RoomService.ts
+++ apps/client/src/lib/functions/services/RoomService.ts
@@ -4,35 +4,13 @@ import { goto } from '$app/navigation';
 import { get } from 'svelte/store';
 import type { RoomCreateOptions } from '$lib/data/RoomCreateOptions';
 
-// function addStateListener(room: Room<RoomState>) {
-//     room.onMessage("pong", (message) => {
-//         console.log("message received from server", message);
-//         console.log(Date.now() - message.time);
-//     });
-//
-//     room.state.players.onAdd((player, key) => {
-//         console.log(key, "has been added to the room");
-//         // add your player entity to the game world!
-//         // If you want to track changes on a child object inside a map, this is a common pattern:
-//         player.onChange(() => {
-//             let gameStates = get(gameStatesStore);
-//             gameStates.set(key, toGameStateDTO(player));
-//         })
-//     });
-//
-//     room.state.listen("isPlaying", (currentValue, previousValue) => {
-//         if (currentValue === previousValue) return;
-//         console.log("isPlaying changed", currentValue);
-//         if (currentValue === true) $inGame = true;
-//         isPlaying = currentValue;
-//     });
-// }
-
 function resetRoom() {
   roomStore.set(null);
   get(playersStore).clear();
   void goto('/multiplayer/');
+  localStorage.removeItem('reconnectionToken');
 }
+
 function addErrorHandling(room: Room) {
   room.onMessage('pong', (message) => {
     // console.log('message received from server', message);
@@ -45,21 +23,38 @@ function addErrorHandling(room: Room) {
   });
   room.onLeave(() => {
     console.log('client left the room');
-    resetRoom();
+    if (get(roomStore)) localStorage.setItem('reconnectionToken', get(roomStore)!.reconnectionToken);
+    roomStore.set(null);
   });
 }
 
 export async function joinRoom(roomId: string) {
   if (get(roomStore)) return;
-  try {
-    const room: Room | undefined = await get(clientStore).joinById(roomId);
-    if (room) {
-      addErrorHandling(room);
-      roomStore.set(room);
+  const reconnectionToken = localStorage.getItem('reconnectionToken');
+
+  let room: Room | null = null;
+
+  if (reconnectionToken) {
+    try {
+      room = await get(clientStore).reconnect(reconnectionToken);
+      console.log('rejoined successfully', room);
+      localStorage.removeItem('reconnectionToken');
+    } catch (e) {
+      console.error('rejoin error', e);
+      resetRoom();
     }
-  } catch (e) {
-    console.error('join error', e);
-    resetRoom();
+  } else {
+    try {
+      room = await get(clientStore).joinById(roomId);
+    } catch (e) {
+      console.error('join error', e);
+      resetRoom();
+    }
+  }
+
+  if (room) {
+    addErrorHandling(room);
+    roomStore.set(room);
   }
 }
 
@@ -78,9 +73,9 @@ export async function createRoom(options: RoomCreateOptions) {
 }
 
 export async function leaveRoom() {
-  await get(roomStore)
-    ?.leave()
-    .then((t: number) => {
-      console.log('left room', t);
-    });
+  const room = get(roomStore);
+  resetRoom();
+  await room?.leave(true).then((t: number) => {
+    console.log('left room', t);
+  });
 }
