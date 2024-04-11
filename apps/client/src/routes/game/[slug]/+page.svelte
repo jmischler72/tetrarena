9254e799c502c2c9b24302977d65afd7eb6a5060
Joffrey <joffrey.mischlerps@gmail.com>
2024-04-11 09:02:04 +0200

    fix(client): fix reconnection problem on chrome
    leave room unconsently on beforeunload to provide the same behaviour of firefox for reconnection

--- apps/client/src/routes/game/[slug]/+page.svelte
+++ apps/client/src/routes/game/[slug]/+page.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-    import {joinRoom} from "$lib/functions/services/RoomService";
+    import { joinRoom } from '$lib/functions/services/RoomService';
     import {roomStore} from "$lib/stores/multiplayerStore";
     import MultiplayerTetris from "./MultiplayerTetris.svelte";
     import WaitingRoom from "./WaitingRoom.svelte";
-    import {onMount} from "svelte";
+    import {  onMount } from 'svelte';
     import MenuWithNavbar from "$lib/components/menu/MenuWithNavbar.svelte";
 
     export let data;
 
     let isPlaying: boolean = false;
 
-    $: $roomStore?.state.listen("isPlaying", (currentValue: boolean) => {
+    $roomStore?.state.listen("isPlaying", (currentValue: boolean) => {
         isPlaying = currentValue;
     });
-
 
     onMount(() => {
         joinRoom(data.slug);
     });
-
 </script>
 
+<svelte:window on:beforeunload={() => $roomStore?.leave(false)}></svelte:window>
 
 {#if $roomStore}
     {#if isPlaying}
