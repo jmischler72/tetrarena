0f5dfa98211481813413d5e60b34a75eb57e9c7a
Joffrey <joffrey.mischlerps@gmail.com>
2024-04-12 13:35:06 +0200

    feat(root): display changelog

--- apps/client/src/routes/(menu)/info/ChangelogMenu.svelte
+++ apps/client/src/routes/(menu)/info/ChangelogMenu.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
 
+	import {marked} from "marked";
+	import changelog from '../../../../../../docs/CHANGELOG.md?raw';
+	import {onMount} from "svelte";
+
+
+	onMount(()=>{
+		console.log(marked.parse(changelog))
+
+		document.getElementById('content').innerHTML  =  <string>marked.parse(changelog);
+	})
+
 </script>
 
 <div class="flex h-full w-full flex-col space-y-4 overflow-y-scroll p-6">
-	<span id="my-container"/>
+	<div id="content" class="markdown"></div>
 </div>
+<style lang="scss">
+	
+	.markdown{
+		font-family: Arial,serif;
+
+		h1{
+			@apply text-2xl;
+		}
+	}
+
+</style>
