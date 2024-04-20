3b3da46f48a7cd274f0e780b1b4c0ed4a301d3af
Joffrey <joffrey.mischlerps@gmail.com>
2024-04-20 12:57:11 +0200

    refactor(client): move snackbar to comp

---
+++ apps/client/src/routes/(menu)/Snackbar.svelte
@@ -0,0 +1,18 @@
+<script>
+import { snackbarStore } from '$lib/stores/snackbarStore';
+
+$: message = $snackbarStore !== '' ? $snackbarStore: message
+$: if ($snackbarStore) setTimeout(() => ($snackbarStore = ''), 3000);
+
+</script>
+
+<div class='w-[40%] flex justify-center rounded-lg absolute bottom-10 right-10 bg-gray-800 transition p-2 z-30'
+     class:opacity-0={$snackbarStore === ''}
+>
+  <div
+    class='w-full h-full flex justify-center rounded border-2 border-solid border-gray-600  py-8  '
+  >
+    <h1 class='text-white'>{message}</h1>
+  </div>
+
+</div>
