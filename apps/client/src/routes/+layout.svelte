78b96d5135d48d4edf408bbfa580b1a1f9a6702a
Joffrey <joffrey.mischlerps@gmail.com>
2024-05-25 13:21:00 +0200

    fix(client): info page + adjust ripple effect

--- apps/client/src/routes/+layout.svelte
+++ apps/client/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import { onMount } from 'svelte';
+	import GridBg from './GridBg.svelte';
 
 	onMount(() => {
 		// setup jdenticon programmatically -> module needed for picker (data-jdenticon-value cant be used with nodejs module)
@@ -14,7 +15,7 @@
 	});
 </script>
 
-<div class="flex h-[100vh] w-[100vw] items-center justify-center bg-gray-800">
+<div class="flex h-[100vh] w-[100vw] items-center justify-center bg-transparent">
 	<slot />
 </div>
 <div class="pointer-events-none absolute left-0 top-0 z-[-1] h-[100vh] w-[100vw] overflow-hidden bg-gray-900">
--- apps/client/src/routes/GridBg.svelte
+++ apps/client/src/routes/GridBg.svelte
@@ -1,79 +1,72 @@
-<script lang='ts'>
-  import { onMount } from 'svelte';
-  import { navigating } from '$app/stores';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { beforeNavigate } from '$app/navigation';
 
-  const colors = [
-    '#FFADAD',
-    '#FFD6A5',
-    '#FDFFB6',
-    '#E4F1EE',
-    '#D9EDF8',
-    '#DEDAF4'
-  ];
+	const colors = ['#FFADAD', '#FFD6A5', '#FDFFB6', '#E4F1EE', '#D9EDF8', '#DEDAF4'];
 
-  let squares: { id: string, color: string, top: number, left: number, classes: string }[] = [];
-  let squareSize = 0;
-  let numSquares = 0;
+	let squares: { id: string; color: string; top: number; left: number; classes: string }[] = [];
+	let squareSize = 0;
+	let numSquares = 0;
 
-  function generateSquares() {
-    squareSize = Math.ceil(
-      Math.min(window.innerWidth, window.innerHeight) / 10
-    );
+	function generateSquares() {
+		squareSize = Math.ceil(Math.min(window.innerWidth, window.innerHeight) / 10);
 
-    numSquares = ((window.innerWidth / squareSize) + 2) * ((window.innerHeight / squareSize) + 2);
-    squares = [];
+		numSquares = (window.innerWidth / squareSize + 2) * (window.innerHeight / squareSize + 2);
+		squares = [];
 
-    for (let i = 0; i < numSquares; i++) {
-      squares.push({
-        id: 'square'+i,
-        color: colors[Math.floor(Math.random() * colors.length)],
-        left: window.innerWidth >= window.innerHeight ? Math.floor(i / 10) * squareSize : (i % 10) * squareSize,
-        top: window.innerWidth >= window.innerHeight ? (i % 10) * squareSize : Math.floor(i / 10) * squareSize,
-        classes: 'opacity-25'
-      });
-    }
-  }
+		for (let i = 0; i < numSquares; i++) {
+			squares.push({
+				id: 'square' + i,
+				color: colors[Math.floor(Math.random() * colors.length)],
+				left: window.innerWidth >= window.innerHeight ? Math.floor(i / 10) * squareSize : (i % 10) * squareSize,
+				top: window.innerWidth >= window.innerHeight ? (i % 10) * squareSize : Math.floor(i / 10) * squareSize,
+				classes: 'opacity-25',
+			});
+		}
+	}
 
-  export const triggerRipple = () => {
-    const middleX = window.innerWidth / 2;
-    const middleY = window.innerHeight / 2;
+	export const triggerRipple = () => {
+		const middleX = window.innerWidth / 2;
+		const middleY = window.innerHeight / 2;
 
-    squares.forEach((square) => {
-      const distanceX = Math.abs(square.left + squareSize / 2 - middleX);
-      const distanceY = Math.abs(square.top + squareSize / 2 - middleY);
-      const distance = Math.max(distanceX, distanceY);
+		squares.forEach((square) => {
+			const distanceX = Math.abs(square.left + squareSize / 2 - middleX);
+			const distanceY = Math.abs(square.top + squareSize / 2 - middleY);
+			const distance = Math.max(distanceX, distanceY);
 
-      const curr_square = document.getElementById(square.id);
+			const curr_square = document.getElementById(square.id);
 
-      setTimeout(() => {
-        curr_square?.classList.add('!opacity-100'); // Add the 'ripple' class to each square to start the animation
+			setTimeout(() => {
+				curr_square?.classList.add('!opacity-30'); // Add the 'ripple' class to each square to start the animation
 
-        setTimeout(() => {
-          curr_square?.classList.remove('!opacity-100'); // Add the 'ripple' class to each square to start the animation
+				setTimeout(() => {
+					curr_square?.classList.remove('!opacity-30'); // Add the 'ripple' class to each square to start the animation
+				}, 300);
+			}, distance * 0.6); // Adjust the delay as per your preference
+		});
+	};
 
-        }, 300);
+	beforeNavigate(() => {
+		triggerRipple();
+	});
 
-      }, distance*0.6); // Adjust the delay as per your preference
-    });
-  };
-
-  $: if($navigating) triggerRipple();
-
-  onMount(() => {
-    window.addEventListener('resize', generateSquares);
-    generateSquares();
-    triggerRipple();
-  });
+	onMount(() => {
+		window.addEventListener('resize', generateSquares);
+		generateSquares();
+		triggerRipple();
+	});
 </script>
+
 {#each squares as square}
-  <div
-    id='{square.id}'
-    class='absolute transition-opacity duration-200 ease-in {square.classes}'
-    style='
+	<div
+		id={square.id}
+		class="absolute transition-opacity duration-200 ease-in {square.classes}"
+		style="
 			width: {squareSize}px;
 			height: {squareSize}px;
 			background-color: {square.color};
 			top: {square.top}px;
 			left: {square.left}px;
-' />
+"
+	/>
 {/each}
