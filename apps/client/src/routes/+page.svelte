4001994088f06fff6a9e2cd2d0536fcb6797aa62
Joffrey <joffrey.mischlerps@gmail.com>
2024-10-30 01:05:40 +0100

    chore(seo): title, meta and links

--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -3,10 +3,16 @@
 </script>
 
 <div class="relative flex h-[50%] w-full flex-col items-center gap-y-10 pb-4 lg:h-[70%]">
-	<h1 class="border-2 border-solid border-white text-2xl text-gray-200 lg:text-5xl">tetrarena</h1>
+	<a class="border-2 border-solid border-white text-2xl text-gray-200 hover:underline lg:text-5xl" href="/info"
+		>tetrarena</a
+	>
 	<Home></Home>
 </div>
 
-<footer class="absolute bottom-5 flex w-[98%] flex-row items-end justify-end gap-2 text-gray-500">
-	<a class="text-lg hover:underline lg:text-3xl" href="/info">@jmischler72</a>
+<footer class="absolute bottom-5 flex w-[98%] flex-row items-center justify-end gap-4 text-gray-500">
+	<a class="text-lg hover:underline lg:text-2xl" href="https://www.youtube.com/@jmischler72-o5d">@jmischler72</a>
+	<p class="text-lg lg:text-3xl">*</p>
+	<a class="text-lg text-gray-300 hover:underline lg:text-xl" href="https://www.patreon.com/c/jmischler72"
+		>help this project</a
+	>
 </footer>
--- apps/client/src/routes/Home.svelte
+++ apps/client/src/routes/Home.svelte
@@ -4,6 +4,7 @@
 
 <div class="relative flex aspect-square h-full flex-col items-center justify-center gap-y-4 text-6xl text-gray-200">
 	<button
+		title="Multiplayer"
 		class="group flex h-1/2 w-full items-center justify-center rounded-xl bg-black bg-opacity-60 transition-shadow hover:shadow-lg hover:shadow-gray-900"
 		on:click={() => goto('/multiplayer')}
 	>
@@ -11,12 +12,14 @@
 	</button>
 	<div class="flex h-1/2 w-full flex-row gap-x-4">
 		<button
+			title="Play"
 			class="group flex w-1/2 items-center justify-center rounded-xl bg-black bg-opacity-60 transition-shadow hover:shadow-lg hover:shadow-gray-900"
 			on:click={() => goto('/game')}
 		>
 			<span class="material-symbols-outlined material-icons !text-7xl group-hover:text-gray-400">play_arrow</span>
 		</button>
 		<button
+			title="Settings"
 			class="group flex w-1/2 items-center justify-center rounded-xl bg-black bg-opacity-60 transition-shadow hover:shadow-lg hover:shadow-gray-900"
 			on:click={() => goto('/settings')}
 		>
